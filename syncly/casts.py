6a78e0ec6237d96bfac2dbfd184882f95fede840
Marc Hoogendoorn <marc.hoogendoorn@live.nl>
2025-08-22 22:06:06 +0200

    cleaned up

--- syncly/casts.py
+++
@@ -1,5 +0,0 @@
-def cast_bool(value):
-    """
-    Converts a string value to a boolean. Returns True if the string is 'true' (case-insensitive), otherwise False.
-    """
-    return value.lower() == 'true'
